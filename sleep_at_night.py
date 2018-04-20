5ff34f0ad6bea24b596611a6caec60e1b75fa42e
Trevor Prater <trevor.prater@gmail.com>
2018-04-20 16:52:00 -0400

    Significant complexity reduction

--- sleep_at_night.py
+++ sleep_at_night.py
@@ -17,32 +17,19 @@ from trader_client import TraderClient
 
 
 def main(sell_threshold=0.04):
-    trader, prices = TraderClient(), {}
+    trader, sell_limit = TraderClient(), {}
 
     while True:
-        assets = trader.get_holdings(avg_price_minutes_ago=1)
-        for currency in set(prices) - {a['currency'] for a in assets}:
-            prices.pop(currency)
+        for asset in trader.get_holdings()
+            currency, price = asset['currency'], asset['price']
 
-        for asset in assets:
-            currency = asset['currency']
-            if currency not in prices:
-                prices[currency] = (-1, -1)
+            sell_limit[currency] = max(
+                    sell_limit.get(currency, -1),
+                    price * (1 - sell_threshold))
 
-            old_floor, old_avg_price = prices[currency]
-            new_floor, new_avg_price = old_floor, asset['avg_price']
-            prices[currency] = (old_floor, new_avg_price)
-
-            if new_avg_price > old_avg_price or old_floor < 0:
-                new_floor = new_avg_price - (new_avg_price * sell_threshold)
-
-                if (new_avg_price - old_floor) > new_avg_price * sell_threshold:
-                    new_floor = new_avg_price - (
-                        new_avg_price * sell_threshold)
-                prices[currency] = (new_floor, new_avg_price)
-
-            if new_avg_price <= new_floor:
+            if price < sell_limit[currency]:
                 trader.sell_all(currency)
+                sell_limit.pop(currency)
 
 
 if __name__ == '__main__':

7992b3ebcf7b680b0420a7e9f49da79d491834fa
Trevor Prater <trevor.prater@gmail.com>
2018-04-20 18:14:06 -0400

    Adds logging

--- sleep_at_night.py
+++ sleep_at_night.py
@@ -12,22 +12,28 @@ As the market price rises, the stop price rises by the trail amount,
 but if the stock price falls, the stop loss price doesn't change,
 and a market order is submitted when the stop price is hit.
 """
+import logging
 from multiprocessing import Pool
 from trader_client import TraderClient
 
 
+LOGGER = logging.getLogger()
 TRADER = TraderClient()
 MAX_DELTA = 0.05
 
 
 def evaluate_asset(asset_and_sell_price):
     asset, sell_price = asset_and_sell_price
-    currency, market_price = asset['currency'], asset['price']
+    currency, market_price = asset['Currency'], asset['AveragePrice']
     sell_price = max(sell_price, market_price * (1 - MAX_DELTA))
+    delta = round((market_price / sell_price)*100.0-100.0, 2)
+    LOGGER.warning("{}: sell_price: {:0.3e}, price: {:0.3e}, delta: {}%".format(
+        currency, sell_price, market_price, delta))
 
     if market_price > sell_price:
         return currency, sell_price
     else:
+        LOGGER.warning("SELLING: {}".format(currency))
         TRADER.sell_all(currency)
         return currency, -1
 
@@ -35,7 +41,7 @@ def evaluate_asset(asset_and_sell_price):
 def main(num_workers=8):
     workers, sell_prices = Pool(num_workers), {}
     while True:
-        asset_data = [(a, sell_prices.get(a['currency'], -1))
+        asset_data = [(a, sell_prices.get(a['Currency'], -1))
                       for a in TRADER.get_holdings()]
 
         for result in workers.imap_unordered(evaluate_asset, asset_data):
@@ -44,6 +50,7 @@ def main(num_workers=8):
                 sell_prices[currency] = sell_price
             else:
                 sell_prices.pop(currency)
+        LOGGER.warning('*'*58)
 
 
 if __name__ == '__main__':
